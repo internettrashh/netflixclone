49fe533c494a11cdb8ae2ba781822e6fdcfecb8b
internet_trash <70342528+internettrashh@users.noreply.github.com>
2024-01-19 03:45:15 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -59,6 +59,7 @@ export default function Navbar({ isScrolled }) {
             signOut(firebaseAuth);
             window.location.href = "/login";
           }}>
+                        <FaPowerOff />
           </button>
         </div>
       </nav>
