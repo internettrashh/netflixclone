961031ab8ab340633ab9bed9a42030e86ff0247f
pk400 <parthkamat10@gmail.com>
2024-01-18 19:40:46 +0530

    signup ui

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import background from '../assets/login.jpg'
+
 function BackgroundImage() {
   return (
     <Container>
-      <img src='{background} ' alt='background'/>
+      <img src={background} alt='background'/>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  img {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
 
 export default BackgroundImage
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function login() {
   return (
     <div>
-      
+      login
     </div>
   )
 }
